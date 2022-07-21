350a8512654c531391d14e7ce642f8081950dde7
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2022-07-21 10:28:31 +0800

    remove contact me section

--- index.py
+++ index.py
@@ -9,35 +9,6 @@ st.write('3. [Covid Dashboard](https://zzahir1978-covid-dashboard-covid-gpsg1z.s
 st.write('4. [User Dashboard](https://zzahir1978-user-dashboard-main-vy308e.streamlitapp.com/)')
 #st.markdown("[![Foo](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
 
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("style.css")
-
-# ---- CONTACT ----
-with st.container():
-    st.write("---")
-    st.subheader("Get In Touch With Me!")
-    st.write("##")
-
-# Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    contact_form = """
-    <form action="https://formsubmit.co/bf85918df6af8e2139d0a995ae42c37a" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Your name" required>
-        <input type="email" name="email" placeholder="Your email" required>
-        <textarea name="message" placeholder="Your message here" required></textarea>
-        <button type="submit">Send</button>
-    </form>
-    """
-left_column, right_column = st.columns(2)
-with left_column:
-    st.markdown(contact_form, unsafe_allow_html=True)
-with right_column:
-    st.empty()
-
 # --- HIDE STREAMLIT STYLE ---
 
 hide_st_style = """
