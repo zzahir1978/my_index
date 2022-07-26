74037d9b2a0b3a1cc66cc1c59a660d96ad0a2a7b
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2022-07-26 14:22:34 +0800

    update web layout and add icons

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config('Zahiruddin Page', ':globe_with_meridians:', layout='wide')
-st.header(':globe_with_meridians:' + " " + 'My Web Page')
-st.subheader('List Of Webpages:-')
-st.write('1. [Main Dashboard](https://share.streamlit.io/zzahir1978/my_dashboard/main/app.py?utm_source=streamlit&utm_medium=zzahir1978@gmail.com&utm_campaign=dashboard)')
-st.write('2. [Covid Dashboard](https://zzahir1978-covid-dashboard-covid-gpsg1z.streamlitapp.com/)')
-st.write('3. [Utilities Dashboard](https://zzahir1978-utility-dashboard-app-pfacnz.streamlitapp.com/?utm_source=streamlit&utm_medium=zzahir1978@gmail.com&utm_campaign=utility+dashboard)')
-st.write('4. [Job Sheet Dashboard](https://zzahir1978-user-dashboard-main-vy308e.streamlitapp.com/)')
+col1, col2, col3 = st.columns(3)
+col2.header(':globe_with_meridians:' + " " + 'My Web Page')
+col2.write("##")
+col2.subheader(':page_facing_up: [Main Dashboard](https://share.streamlit.io/zzahir1978/my_dashboard/main/app.py?utm_source=streamlit&utm_medium=zzahir1978@gmail.com&utm_campaign=dashboard)')
+col2.subheader(':pill: [Covid Dashboard](https://zzahir1978-covid-dashboard-covid-gpsg1z.streamlitapp.com/)')
+col2.subheader(':bust_in_silhouette: [User Dashboard](https://zzahir1978-user-dashboard-main-vy308e.streamlitapp.com/)')
 
 # --- HIDE STREAMLIT STYLE ---
 
